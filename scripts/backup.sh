461ffd212031d52fa37bfb6ee00d0ce40916ef69
Jakob Johnson <33588648+jakobottar@users.noreply.github.com>
2022-07-04 16:57:34 -0600

    added exit status checking and returning

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+# usage: backup.sh rclone-repo:
 
-rclone sync /home/jakobj/samba/ opdi-crypt: --transfers=2 --bwlimit=20M
+rclone sync /home/jakobj/samba/ $1 --transfers=2 --bwlimit=20M
+status=$?
+
+[ $status -eq 0 ] && echo "successfully backed up to $1"
+exit $status
