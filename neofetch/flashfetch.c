7b3859b8df2aad62da0588e05e1e9ace72916c53
jakobottar <jakobottar@pm.me>
2022-09-13 15:07:02 -0600

    💬 update neofetch to flashfetch

---
+++ neofetch/flashfetch.c
@@ -0,0 +1,59 @@
+#include "fastfetch.h"
+// flash/fastfetch from https://github.com/LinusDierheimer/fastfetch
+
+int main(int argc, char** argv)
+{
+    //Disable compiler warnings
+    FF_UNUSED(argc, argv);
+
+    FFinstance instance;
+    ffInitInstance(&instance); //This also applys default configuration to instance.config
+
+    //Modify instance.config here
+
+    ffLoadLogo(&instance); //ffLoadLogoSet(&instance, "my custom logo");
+    ffStrbufSet(&instance.config.color, &instance.config.logoColors[0]); //Use the primary color of the logo as key color
+
+    //Multithreading --> better performance
+    ffStartDetectionThreads(&instance);
+
+    //Does things like disabling line wrap
+    ffStart(&instance);
+
+    //Printing
+    ffPrintTitle(&instance);
+    ffPrintSeparator(&instance);
+    ffPrintOS(&instance);
+    ffPrintHost(&instance);
+    ffPrintKernel(&instance);
+    ffPrintUptime(&instance);
+    //ffPrintProcesses(&instance);
+    ffPrintPackages(&instance);
+    ffPrintShell(&instance);
+    ffPrintResolution(&instance);
+    // ffPrintDesktopEnvironment(&instance);
+    ffPrintWM(&instance);
+    // ffPrintWMTheme(&instance);
+    // ffPrintTheme(&instance);
+    // ffPrintIcons(&instance);
+    // ffPrintFont(&instance);
+    // ffPrintCursor(&instance);
+    ffPrintTerminal(&instance);
+    // ffPrintTerminalFont(&instance);
+    ffPrintCPU(&instance);
+    // ffPrintCPUUsage(&instance);
+    ffPrintGPU(&instance);
+    ffPrintMemory(&instance);
+    ffPrintDisk(&instance);
+    ffPrintBattery(&instance);
+    //ffPrintPlayer(&instance);
+    //ffPrintSong(&instance);
+    //ffPrintLocalIp(&instance);
+    //ffPrintPublicIp(&instance);
+    // ffPrintLocale(&instance);
+    ffPrintBreak(&instance);
+    ffPrintColors(&instance);
+
+    ffFinish(&instance);
+    return 0;
+}
